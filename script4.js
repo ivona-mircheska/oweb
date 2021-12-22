b256bf3bb2aba38f764050e9667fe121676c7b87
Ivona Mirceska <ivonamirceska3@gmail.com>
2021-12-22 02:16:57 +0100

    commit

---
+++ script4.js
@@ -0,0 +1,23 @@
+
+           $(document).ready(function () {
+        
+            
+            $("#home").click(function(){
+           window.location.href="index.html";
+            });    
+            $("#contact").click(function(){
+                window.location.href="index.html";
+                 }); 
+            $("#about").click(function(){
+           window.location.href="about.html";
+            });    
+            $("#gallery").click(function(){
+                window.location.href="gallery.html";
+             }); 
+           
+            
+            $("#blog").click(function(){
+           window.location.href="blog.html";
+            }); 
+    });
+     
